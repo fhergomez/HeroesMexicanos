2df44f66c4e2c57fa7cf47ba3fe3df100b91ee32
fhergomez <fhergomez37@gmail.com>
2016-05-24 00:46:58 -0700

    add wikipedia scraper

--- server/api/imgScraper/index.js
+++ server/api/imgScraper/index.js
@@ -5,4 +5,6 @@ var express = require('express');
 var router = require('express');
 var auth = require('../../auth/auth.service');
 
+router.post('/scrape', auth.isAuthenticated(),controller.scrape);
+
 module.exports = router;
