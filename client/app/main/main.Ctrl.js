bcbf7488a7e349f12acf4e4de65dc241ae09eadd
fhergomez <fhergomez37@gmail.com>
2016-05-26 00:32:32 -0700

    add contoller variables

--- client/app/main/main.Ctrl.js
+++ client/app/main/main.Ctrl.js
@@ -5,10 +5,23 @@
     .module('app')
     .controller('MainCtrl', MainCtrl);
 
-  MainCtrl.$inject = ['$scope', '$state', 'Auth'];
+  MainCtrl.$inject = ['$scope', '$state', 'Auth', '$modal', '$http'];
 
   function MainCtrl($scope, $state, Auth) {
     $scope.user = Auth.getCurrentUser();
+
+    $scope.heroe = {};
+    $scope.scrapePostForm = true;
+    $scope.uploadLookTitle = true;
+    $scope.uploadLookForm = false;
+    $scope.showScrapeDetails = false;
+    $scope.gotScrapeResults = false;
+    $scope.loading = false;
+
+    var myModal = $modal({
+      scope: $scope,
+      show: false
+    });
 
   }
 })();
