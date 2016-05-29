3ee7f7009d126836d176f20e06049c1adb813140
fhergomez <fhergomez37@gmail.com>
2016-05-28 22:38:38 -0700

    moved bower components

--- server/api/heroe/index.js
+++ server/api/heroe/index.js
@@ -5,15 +5,15 @@ var express = require('express');
 var router = express.Router();
 var auth = require('../../auth/auth.service');
 
-router.post('/scrapeUpload', auth.isAuthenticated(), controller.scrapeUpload);
-router.get('/getAllHeroes', controller.allLooks);
-// router.get('/heroe', function(req, res){
-//   Heroe.find({}, function(err, heroes){
-//     if(err){
-//       return res.status(500).err.message(err);
-//     }
-//     res.json({heroes: heroes});
-//   });
-// });
+// router.post('/scrapeUpload', auth.isAuthenticated(), controller.scrapeUpload);
+// router.get('/getAllHeroes', controller.allLooks);
+router.get('/heroe', function(req, res){
+  Heroe.find({}, function(err, heroes){
+    if(err){
+      return res.status(500).err.message(err);
+    }
+    res.json({heroes: heroes});
+  });
+});
 
 module.exports = router;
