3214dca69cbfe646571977553b4dfe9564c3f345
fhergomez <fhergomez37@gmail.com>
2016-05-28 21:49:05 -0700

    modify heroe index.js

--- server/api/heroe/index.js
+++ server/api/heroe/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 var auth = require('../../auth/auth.service');
 
 router.post('/scrapeUpload', auth.isAuthenticated(), controller.scrapeUpload);
-router.get('/getAllHeroes', controller.allLooks);
+router.get('/getAllHeroes', controller.allHeroes);
 // router.get('/heroe', function(req, res){
 //   Heroe.find({}, function(err, heroes){
 //     if(err){
