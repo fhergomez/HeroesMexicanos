08d5166a74083496d0835c2af38f56973c0120b8
fhergomez <fhergomez37@gmail.com>
2016-07-16 11:31:09 -0700

    add admin folder

---
+++ client/app/admin/admin_config.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular.module('app').config(config);
+
+  config.$inject = ['$stateProvider'];
+
+  function config($stateProvider) {
+    $stateProvider.state('admin', {
+      url: '/admin',
+      templateUrl: 'app/admin/admin.html',
+      controller: 'AdminCtrl',
+      authenticate: true
+    });
+  }
+
+})();
